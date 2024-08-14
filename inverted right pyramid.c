6b179c1d2dcd1a73bde009fb68a2e124bfc6b12f
romitha-git <144815096+romitha-git@users.noreply.github.com>
2024-08-14 14:14:11 +0530

    inverted right pyramid.c

---
+++ inverted right pyramid.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+      int rows;
+    printf("Number of rows: ");
+    scanf("%d", &rows);
+
+    for (int i = 0; i <= rows; i++) {
+        for (int j = 0;j<i; j++) 
+        {
+            printf(" ");
+        }
+        for (int k =i;k<= rows;k++) 
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
