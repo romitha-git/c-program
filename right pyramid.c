82b431603fcb8c2635e5b6687c0a8a1853e7bec9
romitha-git <144815096+romitha-git@users.noreply.github.com>
2024-08-14 12:23:59 +0530

    right pyramid.c

---
+++ right pyramid.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+      int rows;
+    printf("Number of rows: ");
+    scanf("%d", &rows);
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1;j<=(rows- i); j++) 
+        {
+            printf(" ");
+        }
+        for (int k =1;k<= i;k++) 
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
